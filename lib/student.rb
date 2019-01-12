2aee2b43737a0fff3ca197d9fa83d9d03669890d
tylerselldesign <tylerselldesign@gmail.com>
2019-01-12 19:52:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,17 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  
+  attr_accessible :name, :grade
+  attr_reader :id 
+  
+  
+  def initialize(id = nil, name, grade)
+    @id = id 
+    @name = name 
+    @grade = grade
+  end
+  
+  
 
 end
